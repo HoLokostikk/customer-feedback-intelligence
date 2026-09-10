a29a8671f4bb74dd00aa81ee725bbf1f0af2a16a
HoLokostikk <kostiantyn.holovskyi.shi.2025@lpnu.ua>
2026-09-10 16:32:55 +0300

    fixing_class_imbalance

--- models/model_selection.py
+++ models/model_selection.py
@@ -7,8 +7,13 @@ from sklearn.utils.class_weight import compute_sample_weight
 from sklearn.svm import LinearSVC
 from xgboost import XGBClassifier
 import pandas as pd
+import numpy as np
+
 
 df = pd.read_csv("../dataset/dataset_clean.csv")
+df = df[df["rating_label"] != "neutral"].copy()
+
+custom_weights = {0: 1.0, 1: 3.0, 2: 1.2}
 
 X = df.drop(columns = ["year", "month", "day", "Reviewer Name", "Country"])
 y = df["rating_label"]
@@ -19,7 +24,7 @@ X_val, X_test, y_val, y_test = train_test_split(
     X_temp, y_temp, test_size=0.5, random_state=42, stratify=y_temp
 )
 
-vectorizer = TfidfVectorizer(max_features = 5000)
+vectorizer = TfidfVectorizer(max_features = 5000, ngram_range = (1,2))
 X_train_vec = vectorizer.fit_transform(X_train["Review Text"])
 X_val_vec = vectorizer.transform(X_val["Review Text"])
 
@@ -28,16 +33,16 @@ y_train_vec = le.fit_transform(y_train)
 y_val_vec = le.transform(y_val)
 
 models = {
-    "Logistic Regression" : LogisticRegression(max_iter = 1000, class_weight="balanced"),
-    "LinearSVC" : LinearSVC(dual = False, class_weight = "balanced"),
+    "Logistic Regression" : LogisticRegression(max_iter = 1000, class_weight=custom_weights),
+    "LinearSVC" : LinearSVC(dual = False, class_weight = custom_weights),
     "XGBoost" : XGBClassifier(eval_metric = 'logloss'),
 }
 
-sample_weights = compute_sample_weight(class_weight = "balanced", y = y_train_vec)
+sample_weights_custom = np.array([custom_weights[y] for y in y_train_vec])
 
 for name, model in models.items():
     if name == "XGBoost":
-        model.fit(X_train_vec, y_train_vec, sample_weight=sample_weights)
+        model.fit(X_train_vec, y_train_vec, sample_weight= sample_weights_custom)
     else:
         model.fit(X_train_vec, y_train_vec)
     preds = model.predict(X_val_vec)
