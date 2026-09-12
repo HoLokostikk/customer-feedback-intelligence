b312cfd71e98381da6bb5e33d437cfe7bdfc1935
HoLokostikk <kostiantyn.holovskyi.shi.2025@lpnu.ua>
2026-09-12 14:08:44 +0300

    saved data for forward usage, updated .gitignore

--- models/finetuning.py
+++ models/finetuning.py
@@ -75,6 +75,18 @@ cm1 = confusion_matrix(grid_svc.best_estimator_.predict(X_test_vec), y_test_vec)
 ConfusionMatrixDisplay(cm1).plot()
 plt.savefig("conf_mat_linsvc.png")
 
+import joblib
+joblib.dump(X_train, "train_data/X_train_clean.pkl")
+joblib.dump(y_train, "train_data/y_train_clean.pkl")
+joblib.dump(X_val, "train_data/X_val_clean.pkl")
+joblib.dump(y_val, "train_data/y_val_clean.pkl")
+joblib.dump(X_test, "train_data/X_test_clean.pkl")
+joblib.dump(y_test, "train_data/y_test_clean.pkl")
+joblib.dump(le, "train_data/label_encoder.pkl")
+joblib.dump(vectorizer, "train_data/vectorizer.pkl")
+joblib.dump(grid_logreg.best_estimator_, "train_data/logreg.pkl")
+
+
 
 
 
